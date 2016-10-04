783a9394f4fabaaec756774d5fbd7d1461a7f169
Daniel Estévez <daniel@destevez.net>
2016-10-04 16:02:00 +0200

    Added some missing licence statements
    The project is GPLv3 although some of the code used is public
    domain.

--- lib/golay24.c
+++ lib/golay24.c
@@ -1,4 +1,7 @@
-/* This is free and unencumbered software released into the public domain.
+/**
+ * Copyright 2016 Daniel Estevez <daniel@destevez.net>.
+ *
+ * This is free and unencumbered software released into the public domain.
  * 
  * Anyone is free to copy, modify, publish, use, compile, sell, or
  * distribute this software, either in source code form or as a compiled
